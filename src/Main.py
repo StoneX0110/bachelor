67a7ca76f1a9fc5b78ae611f3c96c5d54a369562
ge49zel <ge49zel@mytum.de>
2021-06-04 11:19:39 +0200

    - added method to remove all appearances of a noise pattern from a text block - added 'number within round brackets' and 'number ending with full stop' as noise patterns -- for now, these patterns get removed in all text blocks without respect to their logical function

--- src/Main.py
+++ src/Main.py
@@ -2,14 +2,17 @@ import os
 import re
 
 # PdfAct https://github.com/ad-freiburg/pdfact
-# read-in PDF file and convert to TXT
+
+# directory of pdfact.jar
 jarDir = os.getcwd().removesuffix('src') + 'pdfact\\bin\\pdfact.jar'
 
+# directory of pdf to be converted, asked in user prompt
 inputDir = input("Enter a path to the PDF file (please use backslashes '\\', not slashes!): ")
 while not os.path.exists(inputDir):
     print("File could not be found at " + str(inputDir))
     inputDir = input("Please enter a valid path: ")
 
+# directory of output to be stored at, asked in user prompt
 outputDir = input("Enter a path for the output file to be stored at (please use backslashes '\\', not slashes!): ")
 while not os.path.exists(outputDir):
     print("File could not be found at " + str(outputDir))
@@ -17,21 +20,41 @@ while not os.path.exists(outputDir):
 outputDir += '\\' + input("Enter name of output file: ") + '.txt'
 
 print("Converting PDF to plain text...")
-
+# convert pdf to plain text
 os.system('java -jar ' + jarDir + ' ' + inputDir + ' output.txt' + ' --include body')
-
 print("Finished conversion, removing noise...")
 
 input_file = open('output.txt', encoding='UTF-8')
 output = open(outputDir, 'w', encoding='UTF-8')
 
-for line in input_file:
+# patterns of noise
+number_rb = '\([0-9]+\)'  # number within round brackets, e.g. "(1)"
+number_fs = '[0-9]+\.'  # number ending with full stop, e.g. "1."
+# TODO: add letters and roman numbering within round brackets as noise patterns and remove them
+
+# removes a pattern from a line and returns the modified line
+def remove_noise(pattern, line):
+    match = re.search(pattern, line)
+    while match:
+        line = line[:match.start()] + line[match.end():]
+        match = re.search(pattern, line)
+    return line
+
+
+for line in input_file:  # line = one text block in PDF
     if line != '\n':
-        # look for enumeration at the start
-        match = re.match('\([0-9]+\)', line)
+        # remove enumeration at start of each text block
+        match = re.match(number_rb, line)
         if match:
-            # remove starting enumeration and following empty space
             line = line.removeprefix(match.group(0))[1:]
+        match = re.match(number_fs, line)
+        if match:
+            line = line.removeprefix(match.group(0))[1:]
+
+        # remove noise within text blocks
+        line = remove_noise(number_fs, line)
+        line = remove_noise(number_rb, line)
+
     output.write(line)
 
 input_file.close()

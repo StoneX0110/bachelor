7965b826bbc56d824d88c7cdea34e4522057a512
ge49zel <ge49zel@mytum.de>
2021-06-11 09:54:22 +0200

    added detailed names of regulations and newer directives/decisions as noise patterns

--- src/Main.py
+++ src/Main.py
@@ -32,8 +32,10 @@ number_rb = '\([0-9]+\)'  # number within round brackets, e.g. "(1)"
 number_fs = '[0-9]+\.'  # number ending with full stop, e.g. "1."
 letter_rb = ' \(?[a-z]\)'  # lower-case letter within round brackets
 roman_rb = '\([x,i,v]{1,4}\)'  # roman numerals within round brackets, recognizes up to no. 17 (xvii)
-directive = '[0-9]+/[0-9]+/[A-Z]+ '  # detailed name of a directive, e.g. 91/477/EEC
-patterns = (number_fs, number_rb, letter_rb, roman_rb, directive)
+regulation = '\(E?E[CU]\) (No )?[0-9]+/[0-9]+'  # name of a Regulation, e.g. "(EEC) No 2956/84"
+directive_old = '(No )?[0-9]+/[0-9]+/[A-Z]+'  # name of a Directive or Decision before 1 January 2015, e.g. "91/477/EEC"
+directive_new = '(\(EU\)|\(Euratom\)|\(EU, Euratom\)|\(CFSP\)) [0-9]+/[0-9]+'  # name of a Directive or Decision after 1 January 2015, e.g. "(EU) 2016/680"
+patterns = (number_fs, number_rb, letter_rb, roman_rb, regulation, directive_old, directive_new)
 
 
 for line in input_file:  # line = one text block in PDF

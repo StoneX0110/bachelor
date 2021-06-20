7a4bf871b5c742e46851fb33afe749ae3a29f43d
ge49zel <ge49zel@mytum.de>
2021-06-20 13:32:33 +0200

    added option of splitting documents based on their chapters, sections and articles added some TODOs

--- src/Main.py
+++ src/Main.py
@@ -13,21 +13,117 @@ while not os.path.exists(inputDir):
     inputDir = input("Please enter a valid path: ")
 
 # directory of output to be stored at, asked in user prompt
-outputDir = input("Enter a path for the output file to be stored at (please use backslashes '\\', not slashes!): ")
+outputDir = input(
+    "Enter a path to a folder where the output file(s) should be stored at (please use backslashes '\\', not slashes!): ")
 while not os.path.exists(outputDir):
     print("Path '" + str(outputDir) + "' does not exist.")
     outputDir = input("Please enter a valid path: ")
-outputDir += '\\' + input("Enter name of output file: ") + '.txt'
+outputDir += '\\' + input("Enter name of output file(s): ") + '.txt'
+
+# granularity by which the document will be split (by its chapters, articles, etc.) into multiple documents
+granularity = input("Enter granularity of splitting (available options: chapter | section | article | none): ")
+while granularity != "chapter" and granularity != "section" and granularity != "article" and granularity != "none":
+    granularity = input("Please enter a valid option (available options: chapter | section | article | none): ")
 
 print("Converting PDF to plain text...")
-# convert pdf to plain text
-os.system('java -jar ' + jarDir + ' ' + inputDir + ' output.txt' + ' --include body')
-print("Finished conversion, removing noise...")
+# convert pdf to plain text, one raw version with all elements, one with only headings and body elements
+os.system('java -jar ' + jarDir + ' ' + inputDir + ' output_raw.txt')
+os.system('java -jar ' + jarDir + ' ' + inputDir + ' output_filtered.txt' + ' --include-roles heading,body')
 
-input_file = open('output.txt', encoding='UTF-8')
-output = open(outputDir, 'w', encoding='UTF-8')
+print("Finished conversion, splitting document...")
+
+file = open('output_raw.txt', encoding='UTF-8')
+raw = file.read()
+file.close()
+file = open('output_filtered.txt', encoding='UTF-8')
+filtered = file.read()
+file.close()
+
+# TODO split annex from rest of document
+
+
+counter = 0
+if granularity == "chapter" or granularity == "section":
+    # search for all chapters
+    pattern = '\nCHAPTER .+?Article [0-9]+\n'
+    m = re.search(pattern, raw, re.DOTALL)
+    c_beginnings = []  # stores the articles where the chapters start at
+    while m:
+        c_beginnings.append(m.group(0)[m.group(0).find('Article'):])
+        raw = raw[:m.start()] + raw[m.end():]
+        m = re.search(pattern, raw, re.DOTALL)
+
+    if len(c_beginnings) > 0:
+        # create files for each chapter
+        for chapter in c_beginnings:
+            output = open(f'{outputDir}_chapter_{counter}.txt', 'w', encoding='UTF-8')
+            output.write(filtered.split(chapter)[0])
+            filtered = filtered.split(chapter)[1]
+            output.close()
+            counter += 1
+        output = open(f'{outputDir}_chapter_{counter}.txt', 'w', encoding='UTF-8')
+        output.write(filtered)
+        output.close()
+
+        if granularity == "section":
+            for i in range(counter):
+                # search for section at start of chapter
+                pattern = '\n[SECTIONsection ]{7,}.+?Article [0-9]+\n'
+                file = open(f'{outputDir}_chapter_{i + 1}.txt', 'r', encoding='UTF-8')
+                chapter = file.read()
+                file.close()
+                m = re.search(pattern, chapter, re.DOTALL)
+                s_beginnings = []  # stores the articles where the sections start at
+                while m:
+                    s_beginnings.append(m.group(0)[m.group(0).find('Article'):])
+                    chapter = chapter.replace(m.group(0)[:m.group(0).find('Article')], '')
+                    m = re.search(pattern, chapter, re.DOTALL)
+
+                if len(s_beginnings) > 0:
+                    counter2 = 0
+                    # create files for each chapter
+                    for section in s_beginnings:
+                        if counter2 != 0:
+                            output = open(f'{outputDir}_chapter_{i + 1}_section_{counter2}.txt', 'w', encoding='UTF-8')
+                            output.write(chapter.split(section)[0])
+                            chapter = chapter.split(section)[1]
+                            output.close()
+                        counter2 += 1
+                    output = open(f'{outputDir}_chapter_{i + 1}_section_{counter2}.txt', 'w', encoding='UTF-8')
+                    output.write(chapter)
+                    output.close()
+
+                    os.remove(f'{outputDir}_chapter_{i + 1}.txt')
+    else:
+        output = open(f'{outputDir}_chapter_{counter}.txt', 'w', encoding='UTF-8')
+        output.write(filtered)
+        output.close()
+
+elif granularity == "article":
+    # search for all articles
+    pattern = 'Article [0-9]+\n'
+    m = re.search(pattern, raw)
+    beginnings = []  # stores heading of articles (without title)
+    while m:
+        beginnings.append(m.group(0))
+        raw = raw[:m.start()] + raw[m.end():]
+        m = re.search(pattern, raw)
+
+    # create files for each article
+    for article in beginnings:
+        output = open(f'{outputDir}_{granularity}_{counter}.txt', 'w', encoding='UTF-8')
+        output.write(filtered.split(article)[0])
+        filtered = filtered.split(article)[1]
+        output.close()
+        counter += 1
+    output = open(f'{outputDir}_{granularity}_{counter}.txt', 'w', encoding='UTF-8')
+    output.write(filtered)
+    output.close()
+
+print("Removing noise...")
 
 # patterns of noise
+# TODO remove remaining titles of article/chapter/section/sub-section/etc. ('Article [0-9]+\n' or similar)
 number_rb = '\([0-9]+\)'  # number within round brackets, e.g. "(1)"
 number_fs = '[0-9]+\.'  # number ending with full stop, e.g. "1."
 letter_rb = ' \(?[a-z]\)'  # lower-case letter within round brackets
@@ -37,7 +133,9 @@ directive_old = '(No )?[0-9]+/[0-9]+/[A-Z]+'  # name of a Directive or Decision 
 directive_new = '(\(EU\)|\(Euratom\)|\(EU, Euratom\)|\(CFSP\)) [0-9]+/[0-9]+'  # name of a Directive or Decision after 1 January 2015, e.g. "(EU) 2016/680"
 patterns = (number_fs, number_rb, letter_rb, roman_rb, regulation, directive_old, directive_new)
 
-
+# remove noise from each previously created document
+input_file = open(outputDir, encoding='UTF-8')  # TODO implement iteration through all previously splitted documents
+output = open(outputDir, 'w', encoding='UTF-8')  # TODO see above
 for line in input_file:  # line = one text block in PDF
     if line != '\n':
         # remove enumeration at start of each text block

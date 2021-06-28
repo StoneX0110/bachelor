7a062d2b324932a4464e6b2d5b1f7d4e28aeb9dc
ge49zel <ge49zel@mytum.de>
2021-06-28 16:40:59 +0200

    adjusted the splitting of documents to work with new input

--- src/Main.py
+++ src/Main.py
@@ -16,7 +16,7 @@ outputDir = input(
 while not os.path.exists(outputDir):
     print("Path '" + str(outputDir) + "' does not exist.")
     outputDir = input("Please enter a valid path: ")
-output_name = '/' + input("Enter name of output file(s): ") + '.txt'
+outputDir += '/' + input("Enter name of output file(s): ") + '.txt'
 
 # granularity by which the document will be split (by its chapters, articles, etc.) into multiple documents
 granularity = input("Enter granularity of splitting (available options: chapter | section | article | none): ")
@@ -27,8 +27,128 @@ print("Converting PDF to plain text...")
 # convert pdf to plain text
 os.system(f'java -jar pdfbox-app-3.0.0-RC1.jar export:text -i={inputDir}')
 
+print("Finished conversion, splitting document...")
+
+
+# gets thrown when annex is reached when reading the input
+class GetOutOfLoop(Exception):
+    pass
+
+
 input_file = open(inputDir.replace(".pdf", ".txt"), encoding='UTF-8')
-output = open(outputDir + output_name, 'w', encoding='UTF-8')
+counter = 0  # keeps track of chapter/article count
+documents = []  # stores names of all created documents for later processing
+annex = 'ANNEX.+?[ ]*\n'  # pattern to recognize start of annex
+
+if granularity == "chapter" or granularity == "section":
+    pattern = 'CHAPTER .+?[ ]*\n'
+    try:
+        for line in input_file:
+            # store every chapter in its own file
+            output = open(f'{outputDir}_chapter_{counter}.txt', 'w', encoding='UTF-8')
+            # break when new chapter is reached
+            while not re.match(pattern, line) and line != "":
+                if re.match(annex, line, re.DOTALL):
+                    output.close()
+                    raise GetOutOfLoop
+                # leave out all article names
+                if not re.match('Article [0-9]+[ ]*\n', line):
+                    output.write(line)
+                line = input_file.readline()
+            output.close()
+            documents.append(f'{outputDir}_chapter_{counter}.txt')
+            counter += 1
+    except GetOutOfLoop:
+        pass
+
+    if granularity == "section":
+        # if counter > 1 ?
+        # search every chapter for sections
+        for i in range(counter - 1):
+            counter2 = 0  # keeps track of section count
+            pattern = '((S ?E ?C ?T ?I ?O ?N)|(S ?e ?c ?t ?i ?o ?n)).+?[ ]*\n'
+            chapter = open(f'{outputDir}_chapter_{i + 1}.txt', 'r', encoding='UTF-8')
+            for line in chapter:
+                # store every section in its own file
+                output = open(f'{outputDir}_chapter_{i + 1}_section_{counter2}.txt', 'w', encoding='UTF-8')
+                documents.append(f'{outputDir}_chapter_{i + 1}_section_{counter2}.txt')
+                # break when new section is reached
+                while not re.match(pattern, line) and line != "":
+                    output.write(line)
+                    line = chapter.readline()
+                if re.match(pattern, line):
+                    counter2 += 1
+                output.close()
+            chapter.close()
+            # if there were sections in the chapter, the original chapter file gets removed
+            if counter2 > 0:
+                documents.remove(f'{outputDir}_chapter_{i + 1}.txt')
+                os.remove(f'{outputDir}_chapter_{i + 1}.txt')
+                documents.remove(f'{outputDir}_chapter_{i + 1}_section_{0}.txt')
+                os.remove(f'{outputDir}_chapter_{i + 1}_section_{0}.txt')
+            # if there were no sections, we need to remove the document of 'section 0' (it has the same text as the respective chapter document)
+            elif counter2 == 0:
+                documents.remove(f'{outputDir}_chapter_{i + 1}_section_{counter2}.txt')
+                os.remove(f'{outputDir}_chapter_{i + 1}_section_{counter2}.txt')
+
+elif granularity == "article":
+    pattern = 'Article [0-9]+[ ]*\n'
+    try:
+        for line in input_file:
+            # store every article in its own file
+            output = open(f'{outputDir}_article_{counter}.txt', 'w', encoding='UTF-8')
+            documents.append(f'{outputDir}_article_{counter}.txt')
+            # break when new article is reached
+            while not re.match(pattern, line) and line != "":
+                if re.match(annex, line, re.DOTALL):
+                    output.close()
+                    raise GetOutOfLoop
+                output.write(line)
+                line = input_file.readline()
+            counter += 1
+            output.close()
+    except GetOutOfLoop:
+        pass
+
+else:
+    # everything is put into one document
+    output = open(f'{outputDir}.txt', 'w', encoding='UTF-8')
+    documents.append(f'{outputDir}.txt')
+    for line in input_file:
+        if re.match(annex, line, re.DOTALL):
+            output.close()
+            raise GetOutOfLoop
+        else:
+            output.write(line)
+    output.close()
+
+input_file.close()
+os.remove(inputDir.replace(".pdf", ".txt"))
+
+
+# TODO remove all remaining occurrences of TITLE, PART, Chapter, Section, Sub-Section, Article etc. in all documents
+# can be done with re.sub
+def remove_titles(file):
+    input_file = open(file, encoding='UTF-8')
+    document = input_file.read()
+    input_file.close()
+
+    output = open(file, 'w', encoding='UTF-8')
+    document = re.sub('pattern', '', document)
+    output.write(document)
+
+    output.close()
+
+
+# regex to identify page headers
+date = '[0-9]{1,2}.[0-9]{1,2}.[0-9]{2,4}'
+issue = 'L [0-9]+/[0-9]+'
+title = 'Official Journal of the European Union'
+language = 'EN'
+
+punctuation_marks = {',', '.', '!', '?', ';', ':'}
+
+ends_with_comma = False  # stores whether previous line ended with comma
 
 
 def editor(line, footnote):
@@ -36,7 +156,7 @@ def editor(line, footnote):
     # filter out page headers
     if not re.search(date, line) or not re.search(issue, line) or not re.search(title, line) or not re.search(language, line):
 
-        if footnote:  # stores whether it is possible that the next paragraph is a footnote
+        if footnote:  # stores whether it is possible that the current paragraph is a footnote
             paragraph = ""  # stores current paragraph
             # footnotes always start with a number within brackets
             if re.match('\( ?[0-9]+ ?\)', line):
@@ -92,114 +212,18 @@ def editor(line, footnote):
         output.write(line)
 
 
-# regex to identify page headers
-date = '[0-9]{1,2}.[0-9]{1,2}.[0-9]{2,4}'
-issue = 'L [0-9]+/[0-9]+'
-title = 'Official Journal of the European Union'
-language = 'EN'
+# TODO test appropriately
+for document in documents:
+    remove_titles(document)
 
-punctuation_marks = {',', '.', '!', '?', ';', ':'}
+    input_file = open(document, 'w', encoding='UTF-8')
+    for line in document:
+        editor(line, True)
+    input_file.close()
 
-ends_with_comma = False  # stores whether previous line ended with comma
-
-for line in input_file:
-    editor(line, True)
-
-output.close()
-input_file.close()
-os.remove(inputDir.replace(".pdf", ".txt"))
-
-print("Finished conversion, splitting document...")
-
-file = open('output_raw.txt', encoding='UTF-8')
-raw = file.read()
-file.close()
-file = open('output_filtered.txt', encoding='UTF-8')
-filtered = file.read()
-file.close()
-
-# TODO split annex from rest of document
-
-# TODO rework this part because of new text extraction tool
-counter = 0
-if granularity == "chapter" or granularity == "section":
-    # search for all chapters
-    pattern = '\nCHAPTER .+?Article [0-9]+\n'
-    m = re.search(pattern, raw, re.DOTALL)
-    c_beginnings = []  # stores the articles where the chapters start at
-    while m:
-        c_beginnings.append(m.group(0)[m.group(0).find('Article'):])
-        raw = raw[:m.start()] + raw[m.end():]
-        m = re.search(pattern, raw, re.DOTALL)
-
-    if len(c_beginnings) > 0:
-        # create files for each chapter
-        for chapter in c_beginnings:
-            output = open(f'{outputDir}_chapter_{counter}.txt', 'w', encoding='UTF-8')
-            output.write(filtered.split(chapter)[0])
-            filtered = filtered.split(chapter)[1]
-            output.close()
-            counter += 1
-        output = open(f'{outputDir}_chapter_{counter}.txt', 'w', encoding='UTF-8')
-        output.write(filtered)
-        output.close()
-
-        if granularity == "section":
-            for i in range(counter):
-                # search for section at start of chapter
-                pattern = '\n[SECTIONsection ]{7,}.+?Article [0-9]+\n'
-                file = open(f'{outputDir}_chapter_{i + 1}.txt', 'r', encoding='UTF-8')
-                chapter = file.read()
-                file.close()
-                m = re.search(pattern, chapter, re.DOTALL)
-                s_beginnings = []  # stores the articles where the sections start at
-                while m:
-                    s_beginnings.append(m.group(0)[m.group(0).find('Article'):])
-                    chapter = chapter.replace(m.group(0)[:m.group(0).find('Article')], '')
-                    m = re.search(pattern, chapter, re.DOTALL)
-
-                if len(s_beginnings) > 0:
-                    counter2 = 0
-                    # create files for each chapter
-                    for section in s_beginnings:
-                        if counter2 != 0:
-                            output = open(f'{outputDir}_chapter_{i + 1}_section_{counter2}.txt', 'w', encoding='UTF-8')
-                            output.write(chapter.split(section)[0])
-                            chapter = chapter.split(section)[1]
-                            output.close()
-                        counter2 += 1
-                    output = open(f'{outputDir}_chapter_{i + 1}_section_{counter2}.txt', 'w', encoding='UTF-8')
-                    output.write(chapter)
-                    output.close()
-
-                    os.remove(f'{outputDir}_chapter_{i + 1}.txt')
-    else:
-        output = open(f'{outputDir}_chapter_{counter}.txt', 'w', encoding='UTF-8')
-        output.write(filtered)
-        output.close()
-
-elif granularity == "article":
-    # search for all articles
-    pattern = 'Article [0-9]+\n'
-    m = re.search(pattern, raw)
-    beginnings = []  # stores heading of articles (without title)
-    while m:
-        beginnings.append(m.group(0))
-        raw = raw[:m.start()] + raw[m.end():]
-        m = re.search(pattern, raw)
-
-    # create files for each article
-    for article in beginnings:
-        output = open(f'{outputDir}_{granularity}_{counter}.txt', 'w', encoding='UTF-8')
-        output.write(filtered.split(article)[0])
-        filtered = filtered.split(article)[1]
-        output.close()
-        counter += 1
-    output = open(f'{outputDir}_{granularity}_{counter}.txt', 'w', encoding='UTF-8')
-    output.write(filtered)
-    output.close()
 
 print("Removing noise...")
+
 
 # TODO rework this part because of new text extraction tool
 # patterns of noise
